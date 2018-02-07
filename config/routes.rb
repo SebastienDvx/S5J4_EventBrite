c65b74f7b94da24bf5e4686623de10cbba407b83
Sébastien DEVAUX <devsebastien@hotmail.fr>
2018-02-07 18:00:02 +0100

    [ADD] Session

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get 'static_pages/home'
+
+  get '/login', to: 'sessions#new'
+
+  post '/login', to: 'sessions#create'
+
+  delete '/logout', to: 'sessions#destroy'
+
+  resources :users
+
+  root 'static_pages#home'
+
 end
